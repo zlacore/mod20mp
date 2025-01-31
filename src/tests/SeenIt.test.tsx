dfc290817e748919f9a1127f12f81a537b8507e1
Zachary LaCore <zlacore12@gmail.com>
2025-01-30 21:06:19 -0600

    testbug

--- src/tests/SeenIt.test.tsx
+++ src/tests/SeenIt.test.tsx
@@ -9,7 +9,7 @@ describe('SeenIt', () => {
 
     // TODO: emptyMessage is a class, not an id. This test should fail our actions
     // TODO: After confirming the actions work as expected, change the querySelector back to a class and commit the changes. Then confirm the actions allow the pull requests
-    const SeenItContainer = document.querySelector('#emptyMessage') as HTMLHeadingElement;
+    const SeenItContainer = document.querySelector('.emptyMessage') as HTMLHeadingElement;
 
     // Verify the output
     expect(SeenItContainer.innerHTML).toBe("Add films you've already seen here.");
